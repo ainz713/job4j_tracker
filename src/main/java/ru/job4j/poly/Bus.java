6c52e4f7f654c76e73c6b1468842fecee34716d6
ainz <ainz713@gmail.com>
2021-02-04 16:29:58 +0300

    3. Интерфейс Input [#443673] корректировка

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,10 +3,13 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void move() {
+        int coordinataX = 10;
+        int coordinataY = 10;
     }
 
     @Override
     public void passengers(int numberofpassengers) {
+        int allpassengers = numberofpassengers + 1;
     }
 
     @Override
