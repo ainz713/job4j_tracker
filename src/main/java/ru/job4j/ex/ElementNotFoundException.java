037c152f850e89b1357f9248807bfea9eae616c1
ainz <ainz713@gmail.com>
2021-02-06 23:10:26 +0300

    0.4. Пользовательские исключения. [#443693] исправил

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -4,17 +4,18 @@ public class ElementNotFoundException extends Exception {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] != key && value[i] == value[value.length - 1]) {
-                throw new ElementNotFoundException();
-            } else if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
             }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException();
         }
         return rsl;
     }
 
     public static void main(String[] args) {
-        String[] value = {"vasya", "petya", null, null};
+        String[] value = {"vasya", "petya", "masha"};
         try {
             System.out.println(indexOf(value, "masha"));
         } catch (ElementNotFoundException e) {
