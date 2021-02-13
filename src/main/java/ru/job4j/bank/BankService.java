bfe95eed57e26a8b7493a3138e60dec8c029529b
ainz <ainz713@gmail.com>
2021-02-13 19:32:34 +0300

    3. Банковские переводы [#443660]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,57 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+            users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        if (!users.get(user).contains(account)) {
+            users.get(user).add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User key : users.keySet()) {
+            if (key.getPassport().equals(passport)) {
+              return key;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        List<Account> account = users.get(user);
+        if (account != null) {
+            int index = account.indexOf(new Account(requisite, -1));
+            return account.get(index);
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account account = findByRequisite(srcPassport, srcRequisite);
+        Account account2 = findByRequisite(destPassport, destRequisite);
+        if (account == null || account.getBalance() < amount) {
+            return rsl;
+        } else {
+            double b = account.getBalance();
+            double d = account2.getBalance();
+            account2.setBalance(amount + d);
+            account.setBalance(b - amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
