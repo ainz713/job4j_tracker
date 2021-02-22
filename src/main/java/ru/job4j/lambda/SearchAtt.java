4f887fba830b2104d423efd133c346f06bc06d7f
ainz <ainz713@gmail.com>
2021-02-22 13:29:49 +0300

    2.0. Встроенные функциональные интерфейсы. [#443476] исправил

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -8,10 +8,22 @@ import java.util.function.Supplier;
 
 public class SearchAtt {
 
+    public static List<Attachment> filterSize(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+        return SearchAtt.filter(list, x -> x.getSize() > 100);
+    }
+
+    public static List<Attachment> filterName(List<Attachment> list) {
+        List<Attachment> rsl = new ArrayList<>();
+       return SearchAtt.filter(list, x -> x.getName().contains("bug"));
+    }
+
     public static List<Attachment> filter(List<Attachment> list, Predicate<Attachment> predicate) {
         List<Attachment> rsl = new ArrayList<>();
         for (Attachment att : list) {
-            predicate.test(att);
+          if (predicate.test(att)) {
+              rsl.add(att);
+          }
         }
         return rsl;
     }
