9b8dba11d150cb6eb200664fcc362875ed3eb467
ainz <ainz713@gmail.com>
2021-03-07 11:48:16 +0300

    1. Генератор текста

---
+++ src/main/java/ru/job4j/collection/Article.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        HashSet<String> sp1 = new HashSet<>();
+        String[] sp2 = line.split("");
+        Collections.addAll(sp1, sp2);
+        int count = 0;
+        for (String s
+                : sp1) {
+            if (origin.contains(s)) {
+                count++;
+            }
+        }
+        return count == sp1.size();
+    }
+}
+
